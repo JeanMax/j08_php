0be22b1770434fda1741cb7a7717f3325f688857
Dylan LE-BLOND <dle-blon@e2r8p1.42.fr>
2015-04-17 14:58:24 +0200

    Zone Complete

--- ex00/IZone.class.php
+++ ex00/IZone.class.php
@@ -2,22 +2,16 @@
 
 interface IZone
 {
-    /*
-    private $_width;
+/*	private $_width;
     private $_height;
     private $_map; //array
-
-    public static $verbose;
+    private $_p1_ships; //array //seriouslee?
+    private $_p2_ships; //array //seriouslee?*/
     
-    */
-    public __construct(array $kw_arg);
+    public function __construct(array $kw_arg);
 /*
   keys : "width" => int, "height" => int, "obstacle" => array("x" => y))
 */
-
-    public function init_obstacle($arr);
-    public function check_collision($xMin, $xMax, $yMin, $yMax);
-    
 }
 
 ?>
--- ex00/Zone.class.php
+++ ex00/Zone.class.php
@@ -1,71 +1,97 @@
 <?PHP
-
 require_once('IZone.class.php');
-
 class Zone implements IZone
 {
+	public $i = 0;
+	public $j = 0;
 	private $_width;
     private $_height;
     private $_map; //array
 
-	public static $verbose = false;
-
-    //MAGIC
-	public __construct(array $kw_arg)
+	public function __construct(array $kw_arg)
 	{
 		if (array_key_exists("width", $kw_arg))
 			$this->setWidth($kw_arg["width"]);
 		else
 			$this->setWidth(150);
 		if (array_key_exists("height", $kw_arg))
-			$this->setWidth($kw_arg["height"]);
+            $this->setHeight($kw_arg["height"]);
 		else
-			$this->setHeight(100);
+            $this->setHeight(100);
 		if (array_key_exists("obstacle", $kw_arg))
-			$this->init_obstacle($kw_arg["obstacle"]);
+			$this->setMap($this->getHeight(), $this->getWidth(), $kw_arg["obstacle"]);
 		else
-			$this->init_obstacle(array());
-
-		$this->init_Map($this->getHeight, $this->getWidth, $this->getP1_ships, $this->getP2_ships);
-
-		if (self::$verbose)
-			echo "Zone constructed.".PHP_EOL;
+			$this->setMap($this->getHeight(), $this->getWidth(), array());
 	}
-
-	public function __destruct(array $ships)
-	{
-		if (self::$verbose)
-			echo "Zone destructed.".PHP_EOL;
-	}
-
-	//PUBLIC METHOD
-	public function init_obstacle($arr)
-	{
-		//TODO
-	}
-	public function setMap($w, $h, $p1s, $p2s)
-	{
-		//TODO
-	}
-
+	
 	//SET
+
+	public function init_obstacle($obs)
+	{
+		foreach ($obs as $key => $value)
+			$this->_map[$value][$key] = 2;
+	}
+	public function setMap($h, $w, $obs)
+	{
+		$this->_map = array();
+		$this->i = 0;
+		while ($this->i <= $h + 1)
+		{
+			$this->j = 0;
+			$this->_map[$this->i] = array();
+			while ($this->j <= $w + 1)
+			{
+				if ($this->i == 0 || $this->i == $h + 1 || $this->j == 0 || $this->j == $w + 1)
+					$this->_map[$this->i][$this->j] = 1;
+				else
+					$this->_map[$this->i][$this->j] = 0;
+				$this->j++;
+			}
+			$this->i++;
+		}
+		$this->init_obstacle($obs);
+	}
 	public function setWidth($arg)
 	{
 		$this->_width = $arg;
 	}
 	public function setHeight($arg)
-	{
-		$this->_height = $arg;
-	}
+    {
+        $this->_height = $arg;
+    }
 
 	//GET
+	
+	public function getMap()
+	{
+		return $this->_map;
+	}
 	public function getWidth()
-	{
-		return $this->_width;
-	}
+    {
+        return $this->_width;
+    }
 	public function getHeight()
+    {
+        return $this->_height;
+    }
+}
+
+//TEST
+
+$test1 = new Zone(array("obstacle" => array(12 => 2, 5 => 5, 2 => 8), "height" => 10, "width" => 15));
+$i = 0;
+while ($i <= $test1->getHeight() + 1)
+{
+	$j = 0;
+	while ($j <= $test1->getWidth() + 1)
 	{
-		return $this->_height;
+		if ($j == 0)
+			print($test1->getMap()[$i][$j]);
+		else
+			print(" " . $test1->getMap()[$i][$j]);
+		$j++;
 	}
+	print("\n");
+	$i++;
 }
 ?>
